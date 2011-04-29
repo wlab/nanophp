a6c39184a91e20b583aa84c6dc669c8db653f937
Chris Beck <chris@peopleperhour.com>
2011-04-29 16:57:34 +0100

    Updated example project template...

--- project/cli/generate/views/newExampleProjectTemplateClass.php
+++ project/cli/generate/views/newExampleProjectTemplateClass.php
@@ -20,12 +20,15 @@ class PageTemplate extends \nano\core\page\Page {
 	 */
 	protected function preExecute(\nano\core\routing\Routing $routing) {
 		$this->headerWidget = new \project\apps\{{ projectName }}\widgets\header\Header();
+		$this->headerWidget->addStyleSheet('/css/globals/admin.css');
 		$this->headerWidget->addStyleSheet('/css/globals/default.css');
-		$this->headerWidget->addStyleSheet('/css/globals/colorbox-default.css');
+		$this->headerWidget->addStyleSheet('/css/globals/jquery/plugins/ui/'.\nano\core\config\Config::get('jquery_theme').'/jquery-ui-1.8.5.custom.css');
+		$this->headerWidget->addStyleSheet('/css/globals/jquery/plugins/colorbox/colorbox-default.css');
 		$this->headerWidget->addJavaScript('/js/globals/jquery.min.js');
 		$this->headerWidget->addJavaScript('/js/globals/jquery-ui.min.js');
 		$this->headerWidget->addJavaScript('/js/globals/jquery.colorbox-min.js');
 		$this->headerWidget->addJavaScript('/js/globals/core.js');
+		$this->headerWidget->addJavaScript('/js/globals/admin.js');
 	}
 	
 	/**
